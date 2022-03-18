8600a288d33396326bd77379eaddc841956278b3
danlisc <101846718+danlisc@users.noreply.github.com>
2022-03-18 17:30:45 -0400

    Update Test.m
    implemented classification knn function. Currently uses raw FFT data to train. Just a primitive proof of concept, but achieved 16/20 accuracy. See zip for training and test files.

--- Test.m
+++ Test.m
@@ -1,28 +1,55 @@
+clear;
+close all;
+
 cd EN\;                             %change folder to english folder
 ENfiles = dir('*.opus');            %specify files to read
 
-x=zeros(48312,21);
-y=zeros(48312,21);
+x=zeros(1,36234);
+y={};                               %labels for EN, CN
 
 i=1;
 
 for file = ENfiles'
 
-    temp=audioread(file.name);
-    x(:,i)=temp;                    %concatenate audio clip into zero matrix x
+    temp=abs(fft(audioread(file.name,[1,36234])));
+    x(i,:)=temp;                         %concatenate audio clip into zero matrix x
+    y(i,:)={'EN'};
     i=i+1;
 
 end
 
-ESfiles = dir('ES\');               %repeat for spanish
-i=1;
+cd '..\CN\'
+CNfiles = dir('*.opus');                 %repeat for chinese
 
-for file = ESfiles'
+for file = CNfiles'
 
-    temp=audioread(file.name);
-    y(:,i)=temp;
+    temp=abs(fft(audioread(file.name,[1,36234])));
+    x(i,:)=temp;
+    y(i,:)={'CN'};    
     i=i+1;
 
 end
 
+Mdl=fitcknn(x,y,'NumNeighbors', 5, 'Standardize', 1);   %train model
 
+cd ..;
+TESTfiles = dir('*.opus');
+
+test=zeros(1,36234);
+actual = {};
+
+j=1;
+for file = TESTfiles'                               %concatenate test audio
+    if(contains(file.name,'CN'))                    %assigning labels for confusion chart
+        actual(j,:)={'CN'};
+    elseif(contains(file.name,'EN'))
+        actual(j,:)={'EN'};
+    end
+    temp=abs(fft(audioread(file.name,[1,36234])));
+    test(j,:)=temp;  
+    j=j+1;
+
+end
+
+predicted = predict(Mdl,test);                       %predict with model
+confusionchart(actual,predicted)    

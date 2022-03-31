2082e9d551391c16407955aabef476c497095017
danlisc <101846718+danlisc@users.noreply.github.com>
2022-03-30 20:34:34 -0400

    update

--- Test.m
+++ Test.m
@@ -1,55 +1,105 @@
+%% EECS351 Project KNN test
+
 clear;
 close all;
 
 cd EN\;                             %change folder to english folder
-ENfiles = dir('*.opus');            %specify files to read
+ENfiles = dir('**/*.opus');            %specify files to read
 
-x=zeros(1,36234);
-y={};                               %labels for EN, CN
+fftTrain=zeros(1,43200);
+timeTrain=zeros(1,43200);
+mfccTrain=zeros(1232,1);
+trainLabels={};                               %labels for EN, CN
 
 i=1;
 
 for file = ENfiles'
 
-    temp=abs(fft(audioread(file.name,[1,36234])));
-    x(i,:)=temp;                         %concatenate audio clip into zero matrix x
-    y(i,:)={'EN'};
+    temp=audioread(strcat(ENfiles(i).folder,'\',file.name),[1,43200]);
+    timeTrain(i,:)=temp;                         %concatenate audio clip into zero matrix x
+    fftTrain(i,:)=abs(fft(timeTrain(i,:)));
+    trainLabels(i,:)={'EN'};
+    mfcctemp=mfcc(temp,48000);
+    mfccTrain(:,i)=mfcctemp(:);
     i=i+1;
-
+    
 end
 
 cd '..\CN\'
-CNfiles = dir('*.opus');                 %repeat for chinese
+CNfiles = dir('**\*.opus');                 %repeat for chinese
 
 for file = CNfiles'
 
-    temp=abs(fft(audioread(file.name,[1,36234])));
-    x(i,:)=temp;
-    y(i,:)={'CN'};    
+    temp=audioread(strcat(CNfiles(i-length(ENfiles)).folder,'\',file.name),[1,43200]);
+    timeTrain(i,:)=temp;
+    fftTrain(i,:)=abs(fft(timeTrain(i,:)));
+    trainLabels(i,:)={'CN'};   
+    mfcctemp=mfcc(temp,48000);
+    mfccTrain(:,i)=mfcctemp(:);
     i=i+1;
 
 end
 
-Mdl=fitcknn(x,y,'NumNeighbors', 5, 'Standardize', 1);   %train model
+
+mfccTrain=mfccTrain.';
+
+Mdl=fitcsvm(mfccTrain,trainLabels);
 
 cd ..;
 TESTfiles = dir('*.opus');
 
-test=zeros(1,36234);
+testfft=zeros(1,43200);
+testtime=zeros(1,43200);
+mfcctest=zeros(1232,1);
+
 actual = {};
 
 j=1;
 for file = TESTfiles'                               %concatenate test audio
     if(contains(file.name,'CN'))                    %assigning labels for confusion chart
         actual(j,:)={'CN'};
-    elseif(contains(file.name,'EN'))
+    elseif(contains(file.name,'en'))
         actual(j,:)={'EN'};
     end
-    temp=abs(fft(audioread(file.name,[1,36234])));
-    test(j,:)=temp;  
+    temp=audioread(file.name,[1,43200]);
+    testtime(j,:)=temp;  
+    testfft(j,:)=abs(fft(testtime(j,:)));
+    mfcctemp=mfcc(temp,48000);
+    mfcctest(:,j)=mfcctemp(:);
     j=j+1;
 
 end
 
-predicted = predict(Mdl,test);                       %predict with model
+cd Testing\;
+TEST1files = dir('*.opus');
+
+test1fft=zeros(1,43200);
+test1time=zeros(1,43200);
+mfcctest1=zeros(1232,1);
+actual1 = {};
+k=1;
+for file = TEST1files'                               %concatenate test audio
+    if(contains(file.name,'CN'))                    %assigning labels for confusion chart
+        actual1(k,:)={'CN'};
+    elseif(contains(file.name,'en'))
+        actual1(k,:)={'EN'};
+    end
+    temp=audioread(file.name,[1,43200]);
+    test1time(k,:)=temp;  
+    test1fft(k,:)=abs(fft(test1time(k,:)));
+    mfcctemp=mfcc(temp,48000);
+    mfcctest1(:,k)=mfcctemp(:);
+    k=k+1;
+
+end
+
+mfcctest=mfcctest.';
+mfcctest1=mfcctest1.';
+predicted = predict(Mdl,mfcctest);                       %predict with model
 confusionchart(actual,predicted)    
+
+predicted1 = predict(Mdl,mfcctest1);
+figure(2);
+confusionchart(actual1,predicted1);
+
+

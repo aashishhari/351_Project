221e4195b3180dca122e31f154e63d0a1939945b
danlisc <101846718+danlisc@users.noreply.github.com>
2022-04-02 20:05:27 -0400

    Update Test.m

--- Test.m
+++ Test.m
@@ -3,39 +3,44 @@
 clear;
 close all;
 
-cd EN\;                             %change folder to english folder
-ENfiles = dir('**/*.opus');            %specify files to read
+samples = 1000000;
+mfccCount = 14602;
 
-fftTrain=zeros(1,43200);
-timeTrain=zeros(1,43200);
-mfccTrain=zeros(1232,1);
+cd EN\;                             %change folder to english folder
+ENfiles = dir('**/*.mp3');            %specify files to read
+
+fftTrain=zeros(1,samples);
+timeTrain=zeros(1,samples);
+mfccTrain=zeros(mfccCount,1);
 trainLabels={};                               %labels for EN, CN
 
 i=1;
 
 for file = ENfiles'
 
-    temp=audioread(strcat(ENfiles(i).folder,'\',file.name),[1,43200]);
-    timeTrain(i,:)=temp;                         %concatenate audio clip into zero matrix x
-    fftTrain(i,:)=abs(fft(timeTrain(i,:)));
+    temp=audioread(strcat(ENfiles(i).folder,'\',file.name));
+    timeTrain(i,1:length(temp))=temp;                         %concatenate audio clip into zero matrix x
+    fftTrain(i,1:length(timeTrain(i,:)))=abs(fft(timeTrain(i,:)));
     trainLabels(i,:)={'EN'};
     mfcctemp=mfcc(temp,48000);
-    mfccTrain(:,i)=mfcctemp(:);
+    mfcc1d=reshape(mfcctemp.',1,[]);
+    mfccTrain(1:length(mfcc1d),i)=mfcc1d(:);
     i=i+1;
     
 end
 
 cd '..\CN\'
-CNfiles = dir('**\*.opus');                 %repeat for chinese
+CNfiles = dir('**\*.mp3');                 %repeat for chinese
 
 for file = CNfiles'
 
-    temp=audioread(strcat(CNfiles(i-length(ENfiles)).folder,'\',file.name),[1,43200]);
-    timeTrain(i,:)=temp;
-    fftTrain(i,:)=abs(fft(timeTrain(i,:)));
+    temp=audioread(strcat(CNfiles(i-length(ENfiles)).folder,'\',file.name));
+    timeTrain(i,1:length(temp))=temp;                         %concatenate audio clip into zero matrix x
+    fftTrain(i,1:length(timeTrain(i,:)))=abs(fft(timeTrain(i,:)));
     trainLabels(i,:)={'CN'};   
     mfcctemp=mfcc(temp,48000);
-    mfccTrain(:,i)=mfcctemp(:);
+    mfcc1d=reshape(mfcctemp.',1,[]);
+    mfccTrain(1:length(mfcc1d),i)=mfcc1d(:);
     i=i+1;
 
 end
@@ -43,14 +48,14 @@ end
 
 mfccTrain=mfccTrain.';
 
-Mdl=fitcsvm(mfccTrain,trainLabels);
+Mdl=fitcauto(mfccTrain,trainLabels);
 
 cd ..;
-TESTfiles = dir('*.opus');
+TESTfiles = dir('*.mp3');
 
-testfft=zeros(1,43200);
-testtime=zeros(1,43200);
-mfcctest=zeros(1232,1);
+testfft=zeros(1,samples);
+testtime=zeros(1,samples);
+mfcctest=zeros(mfccCount,1);
 
 actual = {};
 
@@ -61,45 +66,20 @@ for file = TESTfiles'                               %concatenate test audio
     elseif(contains(file.name,'en'))
         actual(j,:)={'EN'};
     end
-    temp=audioread(file.name,[1,43200]);
-    testtime(j,:)=temp;  
-    testfft(j,:)=abs(fft(testtime(j,:)));
+    temp=audioread(file.name);
+    testtime(j,1:length(temp))=temp;     
+    testfft(j,1:length(testtime(j,:)))=abs(fft(testtime(j,:)));
     mfcctemp=mfcc(temp,48000);
-    mfcctest(:,j)=mfcctemp(:);
+    mfcctest1d=reshape(mfcctemp.',1,[]);
+    mfcctest(1:length(mfcctest1d),j)=mfcctest1d(:);
+   
     j=j+1;
 
 end
 
-cd Testing\;
-TEST1files = dir('*.opus');
-
-test1fft=zeros(1,43200);
-test1time=zeros(1,43200);
-mfcctest1=zeros(1232,1);
-actual1 = {};
-k=1;
-for file = TEST1files'                               %concatenate test audio
-    if(contains(file.name,'CN'))                    %assigning labels for confusion chart
-        actual1(k,:)={'CN'};
-    elseif(contains(file.name,'en'))
-        actual1(k,:)={'EN'};
-    end
-    temp=audioread(file.name,[1,43200]);
-    test1time(k,:)=temp;  
-    test1fft(k,:)=abs(fft(test1time(k,:)));
-    mfcctemp=mfcc(temp,48000);
-    mfcctest1(:,k)=mfcctemp(:);
-    k=k+1;
-
-end
 
 mfcctest=mfcctest.';
-mfcctest1=mfcctest1.';
 predicted = predict(Mdl,mfcctest);                       %predict with model
 confusionchart(actual,predicted)    
-
-predicted1 = predict(Mdl,mfcctest1);
-figure(2);
-confusionchart(actual1,predicted1);
 
 

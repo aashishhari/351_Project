ee861e6d20131aa3018bb4e7b40a8bf94d8ceb62
Aashish Hari <ashhari@umich.edu>
2022-04-16 11:31:37 -0400

    separated test and train

--- test.m
+++ test.m
@@ -1,2 +1,51 @@
-% separate test and train.
-% SVM (relieff?)? other algorithsm
+%% 351 Language Classification -- Tester
+% KNN of Mel Frequency Cepstral Coefficients
+
+TESTfiles = dir('./TestData/*.mp3');
+
+%TESTfiles = TESTfiles(1:N_train);
+
+%testfft=zeros(1,samples);
+%testtime=zeros(1,samples);
+mfcctest=zeros(mfccCount,1); %create empty test matrices
+deltatest=zeros(mfccCount,1);
+
+actual = {};
+j = 1;
+for file = TESTfiles'                               %concatenate test audio
+    if(contains(file.name,'CN'))                    %assigning ground truth labels for confusion matrix
+        actual(j,:)={'CN'};
+    elseif(contains(file.name,'en'))
+        actual(j,:)={'EN'};
+    elseif(contains(file.name,'hi'))
+        actual(j,:)={'HN'};
+    end
+  
+    [Y, fs] = audioread("./TestData/" + file.name);
+    Y_n = audioNormalization_YW(Y, 0.5); % scales speech by peak value
+    [mfcc_coeffs, delta] = mfcc(Y_n, fs); % computer mfcc @48kHz
+   
+    mfcc_coeffs(1:20,:)=[];
+    delta(1:20,:)=[];        
+   
+    mfcctest1d=reshape(mfcc_coeffs.',1,[]); %change to 1d
+    deltatest1d=reshape(delta.',1,[]);
+
+    mfcctest(1:length(mfcctest1d),j)=mfcctest1d(:); %add to test array
+    deltatest(1:length(deltatest1d),j)=deltatest1d(:);
+   
+    j=j+1;
+end
+
+mfcctest=mfcctest.'; %transpose
+deltatest=deltatest.';
+
+mfcctesttrimmed = zeros(size(TESTfiles,1),1); %empty trimmed mfcc array
+
+for l = 1:1:predictors
+    mfcctesttrimmed(:,l)=mfcctest(:,idx(l)); %populate with most important predictors
+end
+
+%% Plot Model
+predicted = predict(Mdl,mfcctesttrimmed); %predict for test array with trained model
+confusionchart(actual,predicted)    
--- train.m
+++ train.m
@@ -3,14 +3,14 @@
 
 mfccCount = 14602;
 
-N_train = 10; % number of files to train
+N_train = 2; % number of files to train
 ENfiles = dir('./EN/*.mp3'); ENfiles = ENfiles(1:N_train);
 CNfiles = dir('./CN/*.mp3'); CNfiles = CNfiles(1:N_train);
 HNfiles = dir('./HN/*.mp3'); HNfiles = HNfiles(1:N_train);
 
-ENlabels = cell(length(ENfiles), 2); ENlabels(:,1) = {'EN'}; % create labels, change?
-CNlabels = cell(length(CNfiles), 2); CNlabels(:,1) = {'CN'};
-HNlabels = cell(length(HNfiles), 2); HNlabels(:,1) = {'HN'};
+ENlabels = cell(length(ENfiles), 1); ENlabels(:,1) = {'EN'}; % create labels, change?
+CNlabels = cell(length(CNfiles), 1); CNlabels(:,1) = {'CN'};
+HNlabels = cell(length(HNfiles), 1); HNlabels(:,1) = {'HN'};
 trainLabels = [ENlabels; CNlabels; HNlabels];
 
 mfccTrain = zeros(mfccCount, 1);
@@ -22,21 +22,76 @@ i = 1;
 for file=ENfiles'
     [Y, fs] = audioread("./EN/" + file.name); % specify # of samples
     Y = audioNormalization_YW(Y, 0.5); % scales speech by peak value
-    %trainLabels(i, :) = {'EN');
-    [coeffs, delta, deltadelta] = mfcc(Y, fs); % mfcc @ 48kHz
+   
+    [mfcc_coeffs, delta, deltadelta] = mfcc(Y, fs); % mfcc @ 48kHz
     
-    mfcctemp(1:20,:)=[]; %zero first 20 samples to account for weird delta spike
-    deltatemp(1:20,:)=[]; 
-    deltadeltatemp(1:20,:)=[]; 
+    mfcc_coeffs(1:20,:)=[]; %zero first 20 samples to account for weird delta spike
+    delta(1:20,:)=[]; 
+    deltadelta(1:20,:)=[]; 
     
-    mfcc1d=reshape(mfcctemp.',1,[]); %resize array to 1d for training
-    delta1d=reshape(deltatemp.',1,[]);
-    deltadelta1d=reshape(deltadeltatemp.',1,[]);
+    mfcc1d=reshape(mfcc_coeffs.',1,[]); %resize array to 1d for training
+    delta1d=reshape(delta.',1,[]);
+    deltadelta1d=reshape(deltadelta.',1,[]);
     
     mfccTrain(1:length(mfcc1d),i)=mfcc1d(:); %add array to train array
     deltaTrain(1:length(delta1d),i)=delta1d(:);
     deltadeltaTrain=zeros(mfccCount,1);
     i = i + 1;
 end
+for file=CNfiles'
+    [Y, fs] = audioread("./CN/" + file.name); % specify # of samples
+    Y = audioNormalization_YW(Y, 0.5); % scales speech by peak value
+
+    [mfcc_coeffs, delta, deltadelta] = mfcc(Y, fs); % mfcc @ 48kHz
+    
+    mfcc_coeffs(1:20,:)=[]; %zero first 20 samples to account for weird delta spike
+    delta(1:20,:)=[]; 
+    deltadelta(1:20,:)=[]; 
+    
+    mfcc1d=reshape(mfcc_coeffs.',1,[]); %resize array to 1d for training
+    delta1d=reshape(delta.',1,[]);
+    deltadelta1d=reshape(deltadelta.',1,[]);
+    
+    mfccTrain(1:length(mfcc1d),i)=mfcc1d(:); %add array to train array
+    deltaTrain(1:length(delta1d),i)=delta1d(:);
+    deltadeltaTrain=zeros(mfccCount,1);
+    i = i + 1;
+end
+for file=HNfiles'
+    [Y, fs] = audioread("./HN/" + file.name); % specify # of samples
+    Y = audioNormalization_YW(Y, 0.5); % scales speech by peak value
+
+    [mfcc_coeffs, delta, deltadelta] = mfcc(Y, fs); % mfcc @ 48kHz
+    
+    mfcc_coeffs(1:20,:)=[]; %zero first 20 samples to account for weird delta spike
+    delta(1:20,:)=[]; 
+    deltadelta(1:20,:)=[]; 
+    
+    mfcc1d=reshape(mfcc_coeffs.',1,[]); %resize array to 1d for training
+    delta1d=reshape(delta.',1,[]);
+    deltadelta1d=reshape(deltadelta.',1,[]);
+    
+    mfccTrain(1:length(mfcc1d),i)=mfcc1d(:); %add array to train array
+    deltaTrain(1:length(delta1d),i)=delta1d(:);
+    deltadeltaTrain=zeros(mfccCount,1);
+    i = i + 1;
+end
+
+mfccTrain=mfccTrain.';              %transpose matrix for training
+deltaTrain=deltaTrain.';
+
+[idx,weights]=relieff(mfccTrain,trainLabels,5);             %Determine most important KNN predictors
+predictors = 2500;                                          %Decide predictor cutoff
+mfcctraintrimmed = zeros(size(trainLabels,1),1);                            %empty matrix for trimmed mfcc, hard coded
+
+for k = 1:1:predictors
+    mfcctraintrimmed(:,k)=mfccTrain(:,idx(k));              %populate with most important predictors
+end
+
+Mdl=fitcknn(mfcctraintrimmed,trainLabels,"NumNeighbors",5);         %train model
+
+% Cleanup
+clearvars -except mfccCount Mdl
+
 
 

9d97d8c51d6705032c74b549daedf5736ed0f62e
Aashish Hari <ashhari@umich.edu>
2022-04-22 12:06:00 -0400

    update

---
+++ gmdist.m
@@ -0,0 +1,5 @@
+clc; clear; close all;
+load("Data.mat"); % file from shared drive.
+
+% https://www.mathworks.com/help/stats/fitgmdist.html
+% https://www.mathworks.com/help/stats/gmdistribution.html
